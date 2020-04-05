d5c9f6d35cc77b605069cdb87ecc7461cf2824e4
EL93019205 <59168351+EL93019205@users.noreply.github.com>
2020-04-06 00:06:51 +0900

    Revert "Q10"
    This reverts commit 748b02948fe69ffa137c8d61d52c533345e2d5e4.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root "tweets#index"
-  resources :tweets do
-    resources :comments, only: [:create]
-  end
+  resources :tweets
+  resources :comments, only: [:create]
 
   resources :users, only: [:show]
 end
