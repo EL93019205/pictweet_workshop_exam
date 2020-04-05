a537ad7ce815f2a48d8437841e7031b3bbcad9d5
EL93019205 <59168351+EL93019205@users.noreply.github.com>
2020-04-06 00:12:39 +0900

    Revert "Q4"
    This reverts commit aeb7729f799e5a3caa09715ad103ae94155a17de.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,6 @@
 class TweetsController < ApplicationController
 
-  before_action :redirect_to_index, except: :index
+  before_action :redirect_to_index
 
   def index
     @tweets = Tweet.includes(:user).page(params[:page]).per(5).order("created_at DESC")
